b4d8cbe3f9e76d1be079ff81fc6d5de0e53f7f21
Scott Boms <scottboms@gmail.com>
2024-06-14 18:08:28 -0700

    Updating plugin info to utilize the new syntax for Kirby 4.3. Version bumped to 1.1.1.

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 /**
  * Kirby Semantic Markup Plugin
  *
- * @version 1.1.0
  * @author Scott Boms <plugins@scottboms.com>
- * @copyright Scott Boms <plugins@scottboms.com>
  * @link https://github.com/scottboms/kirby-markup
  * @license MIT
 **/
@@ -20,238 +18,243 @@ Kirby\Sane\Html::$allowedTags['s'] = true;
 Kirby\Sane\Html::$allowedTags['samp'] = true;
 Kirby\Sane\Html::$allowedTags['smallcaps'] = true;
 
-Kirby::plugin('scottboms/kirby-markup', [
-  'tags' => [
-    // Abbreviation
-    'abbr' => [
-      'attr' => [
-        'title',
-        'class'
+Kirby::plugin(
+  name: 'scottboms/kirby-markup', 
+  info: [
+    'homepage' => 'https://github.com/scottboms/kirby-markup'
+  ],
+  version: '1.1.1',
+  extends: [
+    'tags' => [
+      // Abbreviation
+      'abbr' => [
+        'attr' => [
+          'title',
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          // tag options
+          $abbr = $tag->abbr;
+          $title = $tag->title;
+          $class = $tag->class;
+
+          if($title == '' && $class == '') {
+            // if no title or class attributes
+            $html .= '<abbr>' . $abbr . '</abbr>';
+          } elseif($title !== '' && $class == '') {
+            // title provided but no class
+            $html .= '<abbr title="' . $title . '">' . $abbr . '</abbr>';
+          } else {
+            $html .= '<abbr title="' . $title . '" class="' . $class . '">' . $abbr . '</abbr>';
+          }
+          return $html;
+        },
       ],
-      'html' => function($tag) {
-        $html = '';
-        // tag options
-        $abbr = $tag->abbr;
-        $title = $tag->title;
-        $class = $tag->class;
 
-        if($title == '' && $class == '') {
-          // if no title or class attributes
-          $html .= '<abbr>' . $abbr . '</abbr>';
-        } elseif($title !== '' && $class == '') {
-          // title provided but no class
-          $html .= '<abbr title="' . $title . '">' . $abbr . '</abbr>';
-        } else {
-          $html .= '<abbr title="' . $title . '" class="' . $class . '">' . $abbr . '</abbr>';
+      // Citation
+      'cite' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $cite = $tag->cite;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<cite>' . $cite . '</cite>';
+          } else {
+            $html .= '<cite class="' . $class . '">' . $cite . '</cite>';
+          }
+          return $html;
         }
-        return $html;
-      },
-    ],
-
-    // Citation
-    'cite' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $cite = $tag->cite;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<cite>' . $cite . '</cite>';
-        } else {
-          $html .= '<cite class="' . $class . '">' . $cite . '</cite>';
+      // Definition
+      'dfn' => [
+        'attr' => [
+          'class',
+          'title'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $dfn = $tag->dfn;
+          $class = $tag->class;
+          $title = $tag->title;
+
+          if($title == '' && $class == '') {
+            // if no title or class attributes
+            $html .= '<dfn>' . $dfn . '</dfn>';
+          } elseif($title !== '' && $class == '') {
+            // title provided but no class
+            $html .= '<dfn title="' . $title . '">' . $dfn . '</dfn>';
+          } else {
+            $html .= '<dfn title="' . $title . '" class="' . $class . '">' . $dfn . '</dfn>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Definition
-    'dfn' => [
-      'attr' => [
-        'class',
-        'title'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $dfn = $tag->dfn;
-        $class = $tag->class;
-        $title = $tag->title;
 
-        if($title == '' && $class == '') {
-          // if no title or class attributes
-          $html .= '<dfn>' . $dfn . '</dfn>';
-        } elseif($title !== '' && $class == '') {
-          // title provided but no class
-          $html .= '<dfn title="' . $title . '">' . $dfn . '</dfn>';
-        } else {
-          $html .= '<dfn title="' . $title . '" class="' . $class . '">' . $dfn . '</dfn>';
+      // Insert
+      'ins' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $ins = $tag->ins;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<ins>' . $ins . '</ins>';
+          } else {
+            $html .= '<ins class="' . $class . '">' . $ins . '</ins>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Insert
-    'ins' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $ins = $tag->ins;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<ins>' . $ins . '</ins>';
-        } else {
-          $html .= '<ins class="' . $class . '">' . $ins . '</ins>';
+      // Mark
+      'mark' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $mark = $tag->mark;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<mark>' . $mark . '</mark>';
+          } else {
+            $html .= '<mark class="' . $class . '">' . $mark . '</mark>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Mark
-    'mark' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $mark = $tag->mark;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<mark>' . $mark . '</mark>';
-        } else {
-          $html .= '<mark class="' . $class . '">' . $mark . '</mark>';
+      // Inline Quote
+      'q' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $q = $tag->q;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<q>' . $q . '</q>';
+          } else {
+            $html .= '<q class="' . $class . '">' . $q . '</q>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Inline Quote
-    'q' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $q = $tag->q;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<q>' . $q . '</q>';
-        } else {
-          $html .= '<q class="' . $class . '">' . $q . '</q>';
+      // Sample
+      'samp' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $samp = $tag->samp;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<samp>' . $samp . '</samp>';
+          } else {
+            $html .= '<samp class="' . $class . '">' . $samp . '</samp>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Sample
-    'samp' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $samp = $tag->samp;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<samp>' . $samp . '</samp>';
-        } else {
-          $html .= '<samp class="' . $class . '">' . $samp . '</samp>';
+      // Small Caps
+      'smallcaps' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $smallcaps = $tag->smallcaps;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<span class="markup__smallcaps">' . $smallcaps . '</span>';
+          } else {
+            $html .= '<span class="' . $class . '">' . $smallcaps . '</span>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Small Caps
-    'smallcaps' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $smallcaps = $tag->smallcaps;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<span class="markup__smallcaps">' . $smallcaps . '</span>';
-        } else {
-          $html .= '<span class="' . $class . '">' . $smallcaps . '</span>';
+      // Strikethrough
+      's' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $s = $tag->s;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<s>' . $s . '</s>';
+          } else {
+            $html .= '<s class="' . $class . '">' . $s . '</s>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Strikethrough
-    's' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $s = $tag->s;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<s>' . $s . '</s>';
-        } else {
-          $html .= '<s class="' . $class . '">' . $s . '</s>';
+      // Subscript
+      'sub' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $sub = $tag->sub;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<sub>' . $sub . '</sub>';
+          } else {
+            $html .= '<sub class="' . $class . '">' . $sub . '</sub>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Subscript
-    'sub' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $sub = $tag->sub;
-        $class = $tag->class;
 
-        if($class == '') {
-          // if no class attributes
-          $html .= '<sub>' . $sub . '</sub>';
-        } else {
-          $html .= '<sub class="' . $class . '">' . $sub . '</sub>';
+      // Superscript
+      'sup' => [
+        'attr' => [
+          'class'
+        ],
+        'html' => function($tag) {
+          $html = '';
+          $sup = $tag->sup;
+          $class = $tag->class;
+
+          if($class == '') {
+            // if no class attributes
+            $html .= '<sup>' . $sup . '</sup>';
+          } else {
+            $html .= '<sup class="' . $class . '">' . $sup . '</sup>';
+          }
+          return $html;
         }
-        return $html;
-      }
-    ],
-
-    // Superscript
-    'sup' => [
-      'attr' => [
-        'class'
       ],
-      'html' => function($tag) {
-        $html = '';
-        $sup = $tag->sup;
-        $class = $tag->class;
-
-        if($class == '') {
-          // if no class attributes
-          $html .= '<sup>' . $sup . '</sup>';
-        } else {
-          $html .= '<sup class="' . $class . '">' . $sup . '</sup>';
-        }
-        return $html;
-      }
-    ],
-
-  ]
+    ]
 ]);
